6883abf32a7c4549ad1da05308bb1ff961cc372f
vicmoola <fumpavictor41@gmail.com>
2026-05-02 00:34:23 +0300

    Add greet_user function to main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Welcome to InterChat!") 
+﻿print("Welcome to InterChat!")
+
+def greet_user(name):
+    print(f"Hello, {name}! Your messages will be translated to Russian.")
+
+greet_user("Friend")
